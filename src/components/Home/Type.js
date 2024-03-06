74feac88b77e193cb662d9fd5b71027e28e3d74c
ahmadswalih <ahmadswalih777@gmail.com>
2024-03-06 16:36:09 +0530

    resovled the spelling mistake

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,7 +7,7 @@ function Type() {
       options={{
         strings: [
           "Developer",
-          "Desginer",
+          "Designer",
           "MERN Stack Developer",
           "Open Source Contributor",
         ],
